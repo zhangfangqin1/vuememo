38de79a4c27ad169299579f62cbe6d391f7deda9
apple <apple@imaccc.local>
2018-02-14 11:19:10 +0800

    bugfixed

--- src/components/NewMemo.vue
+++ src/components/NewMemo.vue
@@ -7,7 +7,6 @@
     <mt-field label="内容" placeholder="文本内容" type="textarea" rows="12" v-model="memo_content"></mt-field>
     <div class="button-group">
       <mt-button plain size="large" class="new-memo" @click.native="handleSubmitBtn" type="primary">确认提交</mt-button>
-      <mt-button plain size="large" class="new-memo" @click.native="handleCancelBtn" type="danger">取消</mt-button>
     </div>
   </div>
 </template>
@@ -22,15 +21,17 @@ import Header from "@/components/Header";
 
 export default {
   name: "NewMemo",
+  components: {
+    Header
+  },
   data: function() {
     return {
       memo_category_id: "",
       memo_title: "",
-      memo_content: ""
+      memo_content: "",
+      titleInputer: null,
+      contentInputer: null
     };
-  },
-  mounted() {
-    this.memo_category_id = this.type[0];
   },
   computed: {
     ...mapState({
@@ -75,8 +76,28 @@ export default {
       this.$router.push({ path: "/" });
     }
   },
-  components: {
-    Header
+  mounted: function() {
+    this.memo_category_id = this.type[0];
+    // 避免输入法挡住输入框
+    this.titleInputer = document.querySelector("input.mint-field-core");
+    this.contentInputer = document.querySelector("textarea.mint-field-core");
+    this.titleInputer.onfocus = e => {
+      setTimeout(() => {
+        this.titleInputer.scrollIntoView(true);
+        this.titleInputer.scrollIntoViewIfNeeded();
+      }, 300);
+    };
+    this.contentInputer.onfocus = e => {
+      setTimeout(() => {
+        this.contentInputer.scrollIntoView(true);
+        this.contentInputer.scrollIntoViewIfNeeded();
+      }, 300);
+    };
+    window.ontouchstart = () => {
+      // 防止再次挡住
+      this.titleInputer.blur();
+      this.contentInputer.blur();
+    };
   }
 };
 </script>
