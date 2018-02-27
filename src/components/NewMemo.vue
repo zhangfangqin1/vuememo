8f4755216cfa6f003930a1668a71cc29a5ca3e72
apple <apple@imaccc.local>
2018-02-27 11:03:52 +0800

    更新发布md格式文档

--- src/components/NewMemo.vue
+++ src/components/NewMemo.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <Header/>
-    <mt-radio title="笔记类型" v-model="memo_category_id" :options="this.$store.state.type">
-    </mt-radio>
-    <mt-field label="标题" placeholder="请输入标题" v-model="memo_title"></mt-field>
-    <mt-field label="内容" placeholder="文本内容" type="textarea" rows="12" v-model="memo_content"></mt-field>
-    <div class="button-group">
-      <mt-button plain size="large" class="new-memo" @click.native="handleSubmitBtn" type="primary">确认提交</mt-button>
+    <div v-if="ifShowPreview">
+      <Preview :doc="{
+        title: this.memo_title,
+        content: this.memo_content
+      }" @handleClose="handleClosePreviewBtn" />
+    </div>
+    <div v-else>
+      <Header/>
+      <mt-radio title="笔记类型" v-model="memo_category_id" :options="this.$store.state.type">
+      </mt-radio>
+      <mt-field label="标题" placeholder="请输入标题" v-model="memo_title"></mt-field>
+      <mt-field label="内容" placeholder="文本内容" type="textarea" rows="12" v-model="memo_content"></mt-field>
+      <div class="button-group">
+        <mt-button plain size="large" class="new-memo" @click.native="handlePreviewBtn" type="default">预览</mt-button>
+        <mt-button plain size="large" class="new-memo" @click.native="handleSubmitBtn" type="primary">确认提交</mt-button>
+      </div>
     </div>
   </div>
 </template>
@@ -18,11 +27,13 @@ import utils from "../utils";
 
 import actionType from "../store/action";
 import Header from "@/components/Header";
+import Preview from "@/components/Preview";
 
 export default {
   name: "NewMemo",
   components: {
-    Header
+    Header,
+    Preview
   },
   data: function() {
     return {
@@ -30,7 +41,8 @@ export default {
       memo_title: "",
       memo_content: "",
       titleInputer: null,
-      contentInputer: null
+      contentInputer: null,
+      ifShowPreview: false
     };
   },
   computed: {
@@ -42,6 +54,12 @@ export default {
     ...mapActions({
       add_memo: actionType.ADD_MEMO
     }),
+    handlePreviewBtn(e) {
+      this.ifShowPreview = true
+    },
+    handleClosePreviewBtn() {
+      this.ifShowPreview = false
+    },
     handleSubmitBtn(e) {
       if (this.memo_title.length === 0 || this.memo_content.length === 0) {
         Toast({
